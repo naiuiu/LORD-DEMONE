e48f79a0a3701fa94a1023ecfaa257c38db49080
naiuiu <iurato76@gmail.com>
2025-07-03 10:06:35 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'lord-demone-cache-v1';
+const FILES_TO_CACHE = [
+  '.',
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => {
+      return resp || fetch(evt.request);
+    })
+  );
+});
